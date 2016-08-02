8f8310fbad26b88f770965fb83eb818ef9b8d8b5
Larry Koubiak <koubiak@skynet.be>
2016-08-02 09:33:02 +0200

    added tblRomPictures and parsing code for thumbnail names

--- hash.py
+++ hash.py
@@ -10,6 +10,7 @@ import regex as re
 def init_database(cursor):
     cursor.execute("CREATE TABLE IF NOT EXISTS tblRomFiles (filename TEXT, size INT, crc32 TEXT, md5 TEXT, sha1 TEXT)")
     cursor.execute("CREATE TABLE IF NOT EXISTS tblRomInfos (name TEXT, description TEXT, year TEXT, manufacturer TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS tblRomPictures (name TEXT, type TEXT)")
     
 def getsha1(filepath):
     sha1 = hashlib.sha1()
@@ -62,6 +63,8 @@ def scan_folder(path,cursor):
             size, crc32, md5, sha1 = getChecksums(os.path.join(path, filename))
             cursor.execute("INSERT INTO tblRomFiles (filename, size, crc32, md5, sha1) VALUES (?,?,?,?,?)",(filename,size,crc32,md5,sha1))
             print filename + " : " + crc32 + " " + md5 + " " + sha1
+        if filename.endswith("png"):
+            cursor.execute("INSERT INTO tblRomPictures (name, type) VALUES (?,?)",(filename[:-4],os.path.basename(path)))
 
 def import_FBA(path,cur):
     datfile = open(path,"r")
@@ -71,14 +74,14 @@ def import_FBA(path,cur):
     headernode = root.find('header')
     for gamenode in root.findall('game'):
         softname = gamenode.get('name')
-        softdesc = gamenode.find('description').text
+        softdesc = re.sub('[\/<>&*?"|:]',"_",gamenode.find('description').text)
         softyear = gamenode.find('year').text
         softmanu = gamenode.find('manufacturer').text
         print softname + " " + softdesc + " " + softyear + " " + softmanu
         cur.execute("INSERT INTO tblRomInfos (name,description,year,manufacturer) VALUES (?,?,?,?)",(softname,softdesc,softyear,softmanu))
   
 def export_dat(path, cur):
-    f = open(path,"w")
+    f = open(path,"wb")
     f.write("clrmamepro (\n")
     f.write("\tname \"FB Alpha - Arcade Games\"\n")
     f.write("\tname \"FB Alpha v0.2.97.38 Arcade Games\"\n")
@@ -93,7 +96,7 @@ def export_dat(path, cur):
         f.write("\tname \"" + game[1] + "\"\n")
         f.write("\tyear " + game[2] + "\n")
         f.write("\tmanufacturer \"" + game[3] + "\"\n")
-        f.write("\trom ( name {0} size {1} crc32 {2} sha1 {3} )\n".format(re.sub("[/\?<>\\:*\|]","_",game[4]),game[5],game[6],game[8]))
+        f.write("\trom ( name {0} size {1} crc32 {2} sha1 {3} )\n".format(game[4],game[5],game[6],game[8]))
         f.write(")\n\n")
     f.close()
 
@@ -104,12 +107,16 @@ con = lite.connect('sqlite/GameFAQs.db')
 cursor = con.cursor()
 init_database(cursor)
 con.commit()
-## cursor.execute("DELETE FROM tblRomInfos")
-## import_FBA(r'FB Alpha v0.2.97.38 (ClrMame Pro XML).dat',cursor)
-## con.commit()
+cursor.execute("DELETE FROM tblRomInfos")
+import_FBA(r'FB Alpha v0.2.97.38 (ClrMame Pro XML).dat',cursor)
+con.commit()
 ## cursor.execute("DELETE FROM tblRomFiles")
 ## scan_folder(r"F:/GameDB/Roms/fba",cursor)
 ## con.commit()
+cursor.execute("DELETE FROM tblRomPictures")
+scan_folder(r"D:/Emu/RetroArch/thumbnails/FB Alpha - Arcade Games/Named_Snaps",cursor)
+scan_folder(r"D:/Emu/RetroArch/thumbnails/FB Alpha - Arcade Games/Named_Titles",cursor)
+con.commit()
 export_dat(r"FB Alpha - Arcade Games.dat",cursor)
 con.close()
 print datetime.now()-starttime
